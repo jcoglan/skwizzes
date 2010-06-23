e7bd8fc1b4e0ebb4015c2293d5758b8ad071ece2
James Coglan <jcoglan@gmail.com>
2010-06-23 18:16:45 +0100

    Logic for applying an automaton rule to an input.

--- cellular_automaton/wolfram.rb
+++ cellular_automaton/wolfram.rb
@@ -2,8 +2,8 @@
 # http://en.wikipedia.org/wiki/Rule_110
 
 module Wolfram
+  
   class Rule
-    
     # Should probably generate these
     NEIGHBORHOODS = [ [0,0,0],
                       [0,0,1],
@@ -28,6 +28,38 @@ module Wolfram
       @active_neighborhoods = active_neighborhoods
     end
     
+    def cell_value(cells, i)
+      (0...cells.size).include?(i) ? cells[i] : 0
+    end
+    
+    def process(input)
+      cells = Cells.new(input)
+      output = cells.neighborhoods.map do |neighborhood|
+        @active_neighborhoods.include?(neighborhood) ? 1 : 0
+      end
+      [0] + output + [0]
+    end
   end
+  
+  class Cells
+    def initialize(values)
+      @values = values
+    end
+    
+    def neighborhoods
+      output = []
+      @values.each_with_index do |cell, i|
+        output << [i-1, i, i+1].map(&method(:value_at))
+      end
+      output
+    end
+    
+  private
+    
+    def value_at(i)
+      (0...@values.size).include?(i) ? @values[i] : 0
+    end
+  end
+  
 end
 
--- cellular_automaton/wolfram_spec.rb
+++ cellular_automaton/wolfram_spec.rb
@@ -19,5 +19,58 @@ describe Wolfram::Rule do
       neighborhoods(3).should == [[0,0,0],[0,0,1]]
     end
   end
+  
+  describe "#transform" do
+    describe "with a single-neighborhood rule" do
+      let(:rule) { Wolfram::Rule.new([ [0,0,0] ]) }
+      
+      it "converts a single-cell entry correctly" do
+        rule.process([0]).should == [0,1,0]
+        rule.process([1]).should == [0,0,0]
+      end
+      
+      it "converts a two-cell entry correctly" do
+        rule.process([0,0]).should == [0,1,1,0]
+        rule.process([0,1]).should == [0,0,0,0]
+        rule.process([1,0]).should == [0,0,0,0]
+        rule.process([1,1]).should == [0,0,0,0]
+      end
+    end
+    
+    describe "with a multi-neighborhood rule" do
+      let(:rule) { Wolfram::Rule.new([ [0,1,1], [1,0,1] ]) }
+      
+      it "converts a single-cell entry correctly" do
+        rule.process([0]).should == [0,0,0]
+        rule.process([1]).should == [0,0,0]
+      end
+      
+      it "converts a two-cell entry correctly" do
+        rule.process([0,0]).should == [0,0,0,0]
+        rule.process([0,1]).should == [0,0,0,0]
+        rule.process([1,0]).should == [0,0,0,0]
+        rule.process([1,1]).should == [0,1,0,0]
+      end
+      
+      it "converts a three-cell entry correctly" do
+        rule.process([0,0,0]).should == [0,0,0,0,0]
+        rule.process([0,0,1]).should == [0,0,0,0,0]
+        rule.process([0,1,0]).should == [0,0,0,0,0]
+        rule.process([0,1,1]).should == [0,0,1,0,0]
+        rule.process([1,0,0]).should == [0,0,0,0,0]
+        rule.process([1,0,1]).should == [0,0,1,0,0]
+        rule.process([1,1,0]).should == [0,1,0,0,0]
+        rule.process([1,1,1]).should == [0,1,0,0,0]
+      end
+    end
+  end
+end
+
+describe Wolfram::Cells do
+  let(:cells) { Wolfram::Cells.new([0,1,1]) }
+  
+  it "provides a list of neighborhoods" do
+    cells.neighborhoods.should == [ [0,0,1], [0,1,1], [1,1,0] ]
+  end
 end
 
